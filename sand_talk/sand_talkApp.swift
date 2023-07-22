bd7cd179181412eafc783ba5b0c3053ef60a755a
Cdreetz <cdreetz@gmail.com>
2023-07-22 08:57:46 -0500

    Initial Commit

---
+++ sand_talk/sand_talkApp.swift
@@ -0,0 +1,17 @@
+//
+//  sand_talkApp.swift
+//  sand_talk
+//
+//  Created by Christian Reetz on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct sand_talkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
